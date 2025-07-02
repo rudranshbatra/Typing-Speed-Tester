7b6a566d4718785e865b6b07ef5abeb430c15313
rudranshbatra <rudranshbatra10@gmail.com>
2025-07-02 14:05:21 +0530

    STT.py

--- STT.py
+++ STT.py
@@ -82,7 +82,7 @@ def test(stdscr):
 
 def main(stdscr):
 
-    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_YELLOW)
+    curses.init_pair(1,curses.COLOR_RED,curses.COLOR_BLACK)
     curses.init_pair(2,curses.COLOR_GREEN,curses.COLOR_BLACK)
     curses.init_pair(3,curses.COLOR_WHITE,curses.COLOR_BLACK)
     start(stdscr)
